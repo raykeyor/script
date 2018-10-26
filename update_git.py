19ddfd19dd4730a3f5dba126541e858891951f8a
raykeyor <raykeyor@163.com>
2018-10-26 15:15:31 +0800

    Update update_git.py

--- update_git.py
+++ update_git.py
@@ -1,11 +1,12 @@
 # !usr/bin/env python3.6  
 # -*- coding: utf-8 -*-
 # Author:lei.tang
+
+import os
 import datetime
 import logging
 import shutil
 import time
-import os
 import paramiko
 import pysftp
 from git import *
@@ -17,12 +18,13 @@ logging.basicConfig(filename='upload.log', level=logging.INFO, filemode='a',
 def update(origin_path):
     try:
         global data
-        dir = os.chdir(os.path.join(origin_path, '******'))
+        dir = os.chdir(os.path.join(origin_path, 'CloudOptimus'))
         data = Git().execute("git pull")
     except Exception as e:
         os.chdir(origin_path)
-        data = Git().execute("git clone http://*******.git")
+        data = Git().execute("git clone http://******.git")
     finally:
+        logging.info(data)
         print(data)
 
 
@@ -54,6 +56,22 @@ def repalce_file(target_path):
                 # os.remove(dst_config)
                 shutil.copy(src_change, dst_change)
             #     shutil.copy(src_config, dst_config)
+
+
+def insertDist():
+    origin_folder = r'E:\dist'
+    target_folder = r'E:\******\static'
+    if os.path.exists(target_folder) and os.path.isdir(target_folder):
+        try:
+            shutil.rmtree(target_folder)
+            print("The static folder is removed successfully ....")
+        except Exception as e:
+            print(e)
+    try:
+        shutil.copytree(origin_folder, target_folder)
+        print("The static folder is copied  successfully ....")
+    except Exception as e:
+        print(e)
 
 
 class Remote_Put:
@@ -104,12 +122,21 @@ class Remote_Put:
                             self.dir_list.insert(-1, i)
                         else:
                             self.dir_list.insert(0, i)
-                    for i in self.dir_list:
+                    for i in sorted(self.dir_list,reverse=True):
                         sftpp.rmdir(i)
             else:
                 pass
             # 遍历本地文件,然后上传到远程server
             for root, dirs, files in os.walk(self.local_dir):
+
+                #先创建本地要上传到server上的所有不存在的目录
+                for dir in dirs:
+                    local_dir = os.path.join(root,dir)
+                    remote_sub_dir = local_dir.replace(self.local_dir, self.remote_dir).replace("\\",'/')
+                    if not sftpp.exists(remote_sub_dir):
+                        sftp.mkdir(remote_sub_dir)
+
+
                 for filespath in files:
                     local_file = os.path.join(root, filespath)
                     logging.info(','.join(('11', '[%s][%s][%s][%s]' % (root, filespath, local_file, self.local_dir))))
@@ -121,8 +148,11 @@ class Remote_Put:
                         if not sftpp.exists(os.path.split(remote_file)[0]):
                             sftp.mkdir(os.path.split(remote_file)[0])
                         sftp.put(local_file, remote_file)
+
                     logging.info("66 upload %s to remote %s" % (local_file, remote_file))
+
             print('77,upload file success %s ' % datetime.datetime.now())
+
             sftpp.close()
             t.close()
         except Exception as e:
@@ -134,49 +164,105 @@ class Remote_Put:
         client.connect(self.hostname, self.port, username=self.username, password=self.password)  # 连接
 
         stdin, stdout, stderr = client.exec_command(self.command)
-        for i in stdout.readlines():
-            print(i.rstrip('\n'))
-        for i in stderr.readlines():
-            print(i.rstrip("\n"))
 
+        for i in stdout:
+            print(i.rstrip('\n'))
+        for i in stderr:
+            print(i.rstrip("\n"))
         client.close()
 
 
 class Config:
-    agent_api = {
-        'hostname': '********',
-        'username': 'root',
-        'password': 'Nginx@29',
-        'port' : 22,
-        'local_dir': r'E:******\agent-api',
-        'remote_dir': r'/root/nginx/agent-api_1.0/agent-api_1.0/',
-        'command': "cd /root/nginx/ && python3.6 arg.py 1 agent-api"
-    }
+    userName = 'root'
+    num = 1
+    hostName = ''
+    password = ''
+    name = ''
+    path = ''
+    remote_dir = ''
 
-    web_api = {
-        'hostname': '********',
-        'username': 'root',
-        'password': 'qaZ!bevKURTKiD2q',
-        'port': 22,
-        'local_dir': r'********\web-api',
-        'remote_dir': r'/home/lei.tang/myapp/web-api_1.0/web-api_1.0/',
-        'command': "cd /home/lei.tang/myapp/ && python3.6 arg.py 0 agent-api"
-    }
+    def cfg(self):
+        cfg = {
+            'hostname': self.hostName,
+            'username': self.userName,
+            'password': self.password,
+            'port': 22,
+            'local_dir': r'E:\******\{}'.format(self.name),
+            'remote_dir': self.remote_dir,
+            'command': "cd {} && python3.6 arg.py {} {}".format(self.path, self.num, self.name)
+        }
+        return cfg
+
+
+class AgentCofig(Config):
+
+    def __init__(self, num):
+        self.num = num
+        self.hostName = '******'
+        self.password = 'Nginx@29'
+        self.name = 'agent-api'
+        self.path = r'/root/nginx/'
+        self.remote_dir = r'/root/nginx/agent-api_1.0/agent-api_1.0'
+
+
+class WebCofig(Config):
+
+    def __init__(self, num):
+        self.num = num
+        self.hostName = '******'
+        self.password = 'qaZ!bevKURTKiD2q'
+        self.name = 'web-api'
+        self.path = r'****/myapp/'
+        self.remote_dir = r'*****/myapp/web-api_1.0/web-api_1.0'
+
+
+class ThreatCofig(Config):
+
+    def __init__(self, num):
+        self.num = num
+        self.hostName = '****'
+        self.password = 'qaZ!bevKURTKiD2q'
+        self.name = 'threat-analysis'
+        self.path = r'/home/lei.tang/myapp/'
+        self.remote_dir = r'****/myapp/threat-analysis_1.0/threat-analysis_1.0'
+
+
+class EngineCofig(Config):
+
+    def __init__(self, num):
+        self.num = num
+        self.hostName = '******'
+        self.password = 'qaZ!bevKURTKiD2q'
+        self.name = 'engine'
+        self.path = r'*****/myapp/CloudOptimus/'
+        self.remote_dir = r'******/myapp/CloudOptimus/engine_1.0/engine_1.0'
+
+
+def remote(config, flag=3):
+    remote = Remote_Put(config['hostname'],
+                        config['username'],
+                        config['password'],
+                        config['port'],
+                        config['local_dir'],
+                        config['remote_dir'],
+                        config['command'])
+    if flag == 0:
+        remote.upload()
+    elif flag == 1:
+        remote.execute()
+    elif flag == 2:
+        remote.upload()
+        remote.execute()
+    else:
+        print("Your request is not been completed ...")
 
 
 if __name__ == '__main__':
     print(datetime.datetime.now())
-    origin_path = r"E:\github"
-    target_path = r"E:\*****_backup"
-    update(origin_path)
-    time.sleep(1)
-    backup(origin_path, target_path)
-    remote = Remote_Put(Config.agent_api['hostname'],
-                        Config.agent_api['username'],
-                        Config.agent_api['password'],
-                        Config.agent_api['port'],
-                        Config.agent_api['local_dir'],
-                        Config.agent_api['remote_dir'],
-                        Config.agent_api['command'])
-    remote.upload()
-    remote.execute()
+    # origin_path = r"E:\github"
+    # target_path = r"E:\*****_backup"
+    # update(origin_path)
+    # time.sleep(1)
+    # backup(origin_path, target_path)
+    # insertDist()
+    remote(WebCofig(4).cfg(),1)
